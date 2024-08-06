0408a0194ca18c3ab4168ef9bb2413b32ed64b4e
Olga16119 <tkachenko93@ukr.net>
2024-08-06 15:40:09 +0200

    added the searh functionality using API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
+function searhCity(city) {
+  let apiKey = "ef0t819co4fbb2a23ffa030681b76557";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function updateWeather(responce) {
+  console.log(responce.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = responce.data.city;
+
+  let temperature = responce.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  let discriptionElement = document.querySelector("#description");
+  discriptionElement.innerHTML = responce.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${responce.data.temperature.humidity}%`;
+
+  let speedWindElement = document.querySelector("#speed");
+  speedWindElement.innerHTML = `${responce.data.wind.speed}km/h`;
+}
+
 function handleSearch(event) {
   event.preventDefault();
-  let cityElement = document.querySelector("#city");
+
   let searchInputElement = document.querySelector("#search-input");
 
-  cityElement.innerHTML = searchInputElement.value;
+  searhCity(searchInputElement.value);
 }
 
 let formElement = document.querySelector("#search-form");
 formElement.addEventListener("submit", handleSearch);
+
+searhCity("Paris")
