36d11d5ce5be28c645d80b7f13f161f09ba75349
Olga16119 <tkachenko93@ukr.net>
2024-09-01 22:01:34 +0200

    added integration from API

--- src/index.js
+++ src/index.js
@@ -63,22 +63,38 @@ function getForecast(city) {
   axios.get(apiUrl).then(displayForecast);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDate()];
+}
+
 function displayForecast(responce) {
   console.log(responce.data);
   let forecast = "";
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecast =
-      forecast +
-      `<div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">☀️</div>
+  responce.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecast =
+        forecast +
+        `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <div >
+            <img class="weather-forecast-icon" src="${
+              day.condition.icon_url
+            }" />
+            </div>
             <div class="weather-forecast-temperatures">
-              <div class="weather-forecast-temperature"><strong>19°</strong></div>
-              <div class="weather-forecast-temperature">9°</div>
+              <div class="weather-forecast-temperature"><strong>${Math.round(
+                day.temperature.maximum
+              )}°</strong></div>
+              <div class="weather-forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}°</div>
             </div>
           </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#weather-forecast");
